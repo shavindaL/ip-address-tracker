f3da61f66465184508e035434970b144deceb349
shavindaL <shavinda724@gmail.com>
2023-02-14 15:01:06 +0530

    Fix linting issues

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <>
             <div className="attribution">
-                Challenge by <a href="https://www.frontendmentor.io?ref=challenge" rel="attribution" target="_blank">Frontend Mentor</a>.
+                Challenge by <a href="https://www.frontendmentor.io?ref=challenge" rel="attribution, noreferrer" target="_blank">Frontend Mentor</a>.
                 Coded by <a href="#">Your Name Here</a>.
             </div>
         </>
